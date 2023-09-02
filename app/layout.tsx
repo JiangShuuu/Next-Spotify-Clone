761d08087cac13abcc2e466ed1673223bf063876
JiangShuuu <globelex65@gmail.com>
2023-09-02 13:15:25 +0800

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import Sidebar from "~/components/Sidebar";
+import ModalProvider from "~/providers/ModalProvider";
 import SupabaseProvider from "~/providers/SupabaseProvider";
 import UserProvider from "~/providers/UserProvider";
+import ToasterProvider from "~/providers/ToasterProvider";
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,13 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import React from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+import useAuthModal from "~/hooks/useAuthModal";
 import Button from "./Button";
+import { useUser } from "~/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -13,9 +19,22 @@ interface HeaderProps {
 }
 
 export default function Header({ children, className }: HeaderProps) {
+  const authModal = useAuthModal();
   const router = useRouter();
-  const handleLogout = () => {
-    //
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // TODO: Reset any playing songs
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+      console.log(error);
+    } else {
+      toast.success("Logged out!");
+    }
   };
 
   return (
@@ -49,21 +68,38 @@ export default function Header({ children, className }: HeaderProps) {
           </button>
         </div>
         <div className="flex items-center justify-between gap-x-4">
-          <>
-            <div>
+          {user ? (
+            <div className="flex items-center gap-x-4">
+              <Button onClick={handleLogout} className="px-6 py-2 bg-white">
+                Logout
+              </Button>
               <Button
-                onClick={() => {}}
-                className="font-medium bg-transparent text-neutral-300"
+                onClick={() => router.push("/account")}
+                className="bg-white "
               >
-                SignUp
+                <FaUserAlt />
               </Button>
             </div>
-            <div>
-              <Button onClick={() => {}} className="px-6 py-2 bg-white ">
-                Log in
-              </Button>
-            </div>
-          </>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="font-medium bg-transparent text-neutral-300"
+                >
+                  SignUp
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="px-6 py-2 bg-white "
+                >
+                  Log in
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
