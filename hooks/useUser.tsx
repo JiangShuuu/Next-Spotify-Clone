c702020b1667761d277747b547395d32954b7177
JiangShuuu <globelex65@gmail.com>
2023-09-03 12:30:58 +0800

    upload file

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,10 +1,11 @@
-import { User } from '@supabase/auth-helpers-nextjs'
+import { useEffect, useState, createContext, useContext } from 'react'
 import {
-  useSessionContext,
   useUser as useSupaUser,
+  useSessionContext,
+  User,
 } from '@supabase/auth-helpers-react'
-import { createContext, useState, useEffect, useContext } from 'react'
-import { Subscription, UserDetails } from '~/types'
+
+import { UserDetails, Subscription } from '~/types'
 
 type UserContextType = {
   accessToken: string | null
@@ -26,10 +27,9 @@ export const MyUserContextProvider = (props: Props) => {
     isLoading: isLoadingUser,
     supabaseClient: supabase,
   } = useSessionContext()
-
   const user = useSupaUser()
   const accessToken = session?.access_token ?? null
-  const [isLoadingData, setIsLoadingData] = useState(false)
+  const [isLoadingData, setIsloadingData] = useState(false)
   const [userDetails, setUserDetails] = useState<UserDetails | null>(null)
   const [subscription, setSubscription] = useState<Subscription | null>(null)
 
@@ -43,22 +43,19 @@ export const MyUserContextProvider = (props: Props) => {
 
   useEffect(() => {
     if (user && !isLoadingData && !userDetails && !subscription) {
-      setIsLoadingData(true)
-
+      setIsloadingData(true)
       Promise.allSettled([getUserDetails(), getSubscription()]).then(
         (results) => {
           const userDetailsPromise = results[0]
           const subscriptionPromise = results[1]
 
-          if (userDetailsPromise.status === 'fulfilled') {
+          if (userDetailsPromise.status === 'fulfilled')
             setUserDetails(userDetailsPromise.value.data as UserDetails)
-          }
 
-          if (subscriptionPromise.status === 'fulfilled') {
+          if (subscriptionPromise.status === 'fulfilled')
             setSubscription(subscriptionPromise.value.data as Subscription)
-          }
 
-          setIsLoadingData(false)
+          setIsloadingData(false)
         }
       )
     } else if (!user && !isLoadingUser && !isLoadingData) {
@@ -81,7 +78,7 @@ export const MyUserContextProvider = (props: Props) => {
 export const useUser = () => {
   const context = useContext(UserContext)
   if (context === undefined) {
-    throw new Error('useUser must be used within a MyUserContextProvider')
+    throw new Error(`useUser must be used within a MyUserContextProvider.`)
   }
   return context
 }
