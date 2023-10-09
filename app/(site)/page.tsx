49cc9786979c0744099b6324c690ce50abd39163
JiangShuuu <globelex65@gmail.com>
2023-10-09 13:29:24 +0800

    upd stripe

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,13 +7,13 @@ export const revalidate = 0
 
 export default async function Home() {
   const songs = await getSongs()
-  console.log('get', songs)
+
   return (
-    <div className='h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900'>
+    <div className='w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900'>
       <Header>
         <div className='mb-2'>
           <h1 className='text-3xl font-semibold text-white'>Welcome back</h1>
-          <div className='sm:frid-cols-2 mt-4 grid grid-cols-1 gap-3 xl:grid-cols-3 2xl:grid-cols-4'>
+          <div className='grid grid-cols-1 gap-3 mt-4 sm:frid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
             <ListItem
               name='Liked Songs'
               href='liked'
@@ -22,7 +22,7 @@ export default async function Home() {
           </div>
         </div>
       </Header>
-      <div className='mb-7 mt-2 px-6'>
+      <div className='px-6 mt-2 mb-7'>
         <div className='flex items-center justify-between'>
           <h1 className='text-2xl font-semibold text-white'>Newest songs</h1>
         </div>
